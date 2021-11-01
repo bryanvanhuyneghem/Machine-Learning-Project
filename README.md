4b9edeca08546f5df0b3fd51b7a7dbd21e68f70d
Bryan Van Huyneghem <bryan.1376@hotmail.com>
2021-11-01 16:25:32 -0400

    Added ML Project files

---
+++ README.md
@@ -0,0 +1,20 @@
+# Machine Learning Project for ML6
+
+Please find in this repo:
+
+
+* Logbooks
+
+* Runs for sprint 1,2 and 3
+
+* The Doc2Vec notebook
+
+* the lemmatization script
+
+* The main notebook (main_notebook_ml_project.ipynb)
+
+* The final presentation (ml_final_presentation.pptx)
+
+* The 3 notebooks used to balance and clean the data set (rbc_notebook_sprintX.ipynb)
+
+* Notebooks for BERT can be found under runs_Sprint3/BERT
---
+++ runs_Sprint1/runs_SVM/svm_runs_with_issue/lemmatizers/lemmatization2.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('wordnet')
+nltk.download('punkt')
+
+# read in the cleaned file
+df_selected = pd.read_pickle('corpus_balanced2_cleaned_first_cut.pkl')
+
+lemmatizer = WordNetLemmatizer()
+i = 0
+
+
+
+# Tokenize the complaint
+for ind, complaint in df_selected["Consumer complaint narrative"].items():
+    i = i+1
+    words = nltk.word_tokenize(complaint)
+    new_words = []
+    # Lemmatize the words in the complaint
+    for word in words:
+        new_words.append(lemmatizer.lemmatize(word))
+    s = ' '.join(new_words)
+    #print(ind, "(old): ", df_selected["Consumer complaint narrative"][ind])
+    df_selected["Consumer complaint narrative"][ind] = s
+    #print(ind, ": ", s)
+    #print("**********************************************************")
+    if (i % 1000) == 0:
+        print(i)
+    # if i == 10:
+    #     break
+
+#print(df_selected["Consumer complaint narrative"])
+print("nulls in df_selected:", df_selected["Consumer complaint narrative"].isnull().sum())
+
+# if for some reason, some complaints are null; remove them, because there aren't very many of them anyway
+df_bcl = df_selected.dropna()
+
+# pickle for later use
+df_selected.to_csv("corpus_balanced3_cleaned_lemmatized_first_cut.csv", index=False)
